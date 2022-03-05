188f371ee1737d2dcf27f80e581d6e0c0f62cfc6
BenHamoudaHichem <hichembenhamouda11@gmail.com>
2022-03-05 13:39:27 +0100

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/public/home/home.component';
+import { AboutUsComponent } from './components/public/about-us/about-us.component';
+import { RegisterComponent } from './components/public/register/register.component';
+import { LoginComponent } from './components/public/login/login.component';
+import { NavBarComponent } from './components/public/additional/nav-bar/nav-bar.component';
+import { FooterComponent } from './components/public/additional/footer/footer.component';
+import { ContactUSComponent } from './components/public/additional/contact-us/contact-us.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    AboutUsComponent,
+    RegisterComponent,
+    LoginComponent,
+    NavBarComponent,
+    FooterComponent,
+    ContactUSComponent
   ],
   imports: [
     BrowserModule,
