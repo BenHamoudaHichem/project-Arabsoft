cfa7c19334e9ed399c268d010acdbd5d2ca8d41f
Takwa Assaibi <takwaassaibi08@gmail.com>
2022-03-24 22:34:17 +0100

    update nav css

--- src/app/services/validation/DateValidation.ts
+++ src/app/services/validation/DateValidation.ts
@@ -3,12 +3,9 @@ import * as moment from 'moment';
 export class DateValidation {
   static DateConfirmation(control: string, date_now: Date) {
     return (formGroup: FormGroup) => {
-
-    let selectedDate = moment(formGroup.controls[control].value);
- 
-    if (selectedDate.isBefore(date_now) ) {
-      return { DateConfirmation: true };
-    }
-    return null;
+      let selectedDate = moment(formGroup.controls[control].value);
+      if (selectedDate.isBefore(date_now)) {return { DateConfirmation: true };}
+      return null;
+    };
   }
-}}
+}
