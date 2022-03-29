5df4d8c01c5051f9dc5f66df8077fa44a62c8c6e
Takwa Assaibi <takwaassaibi08@gmail.com>
2022-03-29 01:03:21 +0100

    update profil customer

--- src/app/components/customer/customer-profil/customer-profil.component.ts
+++ src/app/components/customer/customer-profil/customer-profil.component.ts
@@ -1,4 +1,9 @@
+import { HttpErrorResponse } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { Report } from 'notiflix';
+import { CookiesService } from 'src/app/services/cookies.service';
+import { IUser } from 'src/app/services/user/iuser';
+import { UserService } from 'src/app/services/user/user.service';
 
 @Component({
   selector: 'app-customer-profil',
@@ -6,10 +11,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./customer-profil.component.css']
 })
 export class CustomerProfilComponent implements OnInit {
-
-  constructor() { }
+id!:string
+user!:IUser
+  constructor( private cookiesServices:CookiesService,private userService:UserService) { }
 
   ngOnInit(): void {
+    this.getUser()
   }
+getUser()
+{
+ this.id=this.cookiesServices.getIdentifier
+ this.userService.getUser(this.id).subscribe((data:IUser)=>
+{
+this.user=data
+console.log(this.user)
+}),
+(error:HttpErrorResponse)=>{
+  Report.failure("Error getting user",error.message,"OK")
+}}
 
 }
