643ba9a91bcaa16538a7a067b92eb9bb1ed6418a
Hichem Ben Hamouda <94752266+BenHamoudaHichem@users.noreply.github.com>
2022-04-23 09:07:10 +0200

    commit lang

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,8 +11,9 @@ export class AppComponent {
   constructor(public translate: TranslateService,@Inject(SESSION_STORAGE) private storage: StorageService) {
     // Register translation languages
     translate.addLangs(['en', 'fr']);
+
     // Set default language
-    storage.set("Lang",storage.get("Lang") || window.navigator.language)
+    storage.set("Lang",storage.get("Lang") || String(window.navigator.language).substring(0,2))
     translate.use(storage.get("Lang"));
 
 
